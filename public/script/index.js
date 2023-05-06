67981b29bb178ea54ead3b7eaa89ed4a886daba3
alebruni <100190727+alebruni@users.noreply.github.com>
2023-05-06 20:44:19 +0200

    bo
    il mio pomeriggio passato così

--- public/script/index.js
+++ public/script/index.js
@@ -4,13 +4,25 @@ async function loadAppalti(){
     listAppalti=await (await fetch("/appalti")).json()
 }
 
-/*function generateTable(list)
+function generateTable(list)
 {
-
-}*/
+    document.getElementById("dati").innerHTML=`
+        <table>
+        <thead>
+            ${Object.keys(list[0]).reduce((tot,v)=>tot+="<th>"+"<div><div class='table_arrow'><span>boh</span></div>"+"<div>"+v+"<div/></div>"+"</th>","")}
+        </thead>
+        <tbody>
+            ${list.reduce(function(tot,v){
+                tot+="<tr>"+Object.keys(v).reduce((final,el)=>final+="<td>"+v[el]+"</td>","")+"</tr>"
+                return tot;
+            },"")}
+        </tbody>
+        </table>
+    `
+}
 
 window.addEventListener("load",async function(){
     await loadAppalti();
-    //generateTable(listAppalti);
+    generateTable(listAppalti);
 })
 
--- server.js
+++ server.js
@@ -13,19 +13,65 @@ app.set('appName', "Appalti");
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
-app.use(cors({origin: '*'}));
+app.use(cors({
+    origin: '*'
+  }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get("/", function(req,res) {
     res.sendFile('index.html');
 });
+
 app.get("/appalti", function(req,res) {
     res.json(appalti);
 });
+
+app.get('/appalti/CIG/:cig', function(req, res){
+    const cig = req.params.cig;
+    const filteredAppalti = appalti.filter(appalto => appalto["CIG"] === cig.toUpperCase());
+    res.json(filteredAppalti);
+});
+
+app.get('/appalti/dataAdOggi/:data', function(req,res){
+    const data = new Date(req.params.data);
+    const filteredAppalti = appalti.filter(appalto => new Date(appalto["DATA PUBBLICAZIONE"]) >= data);
+    res.json(filteredAppalti);
+});
+
+app.get('/appalti/data/:data', function(req,res){
+    const data = req.params.data;
+    const filteredAppalti = appalti.filter(appalto => appalto["DATA PUBBLICAZIONE"] === data);
+    res.json(filteredAppalti);
+});
+
+app.get('/appalti/valBaseAsta/:minmax', function(req,res){
+    const [min, max] = req.params.minmax.split('-').map(val => parseInt(val));
+    const filteredAppalti = appalti.filter(appalto => appalto["VALORE A BASE D'ASTA"] >= min && appalto["VALORE A BASE D'ASTA"] <= max);
+    res.json(filteredAppalti);
+});
+
+app.get('/appalti/loc/:loc', function(req,res){
+    let loc = req.params.loc;
+    loc=loc.replaceAll("_"," ");
+    const filteredAppalti = appalti.filter(appalto => appalto["LOCALIZZAZIONE"].toLowerCase() === loc.toLowerCase());
+    res.json(filteredAppalti);
+});
+
+app.get('/appalti/contratto/:contratto', function(req,res){
+    const contratto = req.params.contratto;
+    const filteredAppalti = appalti.filter(appalto => appalto["TIPOLOGIA CONTRATTO"].toLowerCase() === contratto.toLowerCase());
+    res.json(filteredAppalti);
+});
+
+
 app.use("*", function(req,res,next){
     res.status(404); 
     res.send('Url non presente');
-}); 
+});
+
+    
 const server = app.listen(app.get('port'), function(){
     console.log('Server in ascolto');
 
